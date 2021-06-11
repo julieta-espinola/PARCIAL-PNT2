f6eb77dbf1944ddf9ade4b7437b963f0efa2c594
Espinola <jespinola@grupoassa.com>
2021-06-10 22:26:41 -0300

    PARCIAL PNT2

---
+++ src/router.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Formulario from './components/Formulario.vue'
+
+import Tabla from './components/Tabla.vue'
+Vue.use(VueRouter)
+
+export const router = new VueRouter({
+    mode: 'history',
+    routes : [
+        { path: '/', redirect:'/binding' },
+        { path: '/formulario', component: Formulario },
+        {path: '/tabla',component:Tabla},
+     
+    ]
+})
